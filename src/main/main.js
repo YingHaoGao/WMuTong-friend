0d88f62dae0649e2df5aba87c1b83b86181e6f66
gaoyinghao <gaoyinghao@hulujianyi.com>
2020-08-20 14:51:02 +0800

    展示3D模型成功

---
+++ src/main/main.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow, screen } = require('electron');
+const path = require('path');
+const fs = require('fs');
+const env = process.env;
+
+function createWindow() {
+	let width = 400,
+		height = 500,
+		workAreaSize = screen.getPrimaryDisplay().workAreaSize,
+		x = workAreaSize.width - width,
+		y = workAreaSize.height - height,
+		alwaysOnTop = true;
+
+	if(env.NODE_ENV == 'devTools') {
+		width = 1600;
+		height = 800;
+		x = workAreaSize.width - width;
+		y = workAreaSize.height - height;
+		alwaysOnTop = false;
+	}
+
+	let win = new BrowserWindow({
+		width: width,
+		height: height,
+		x: x,
+		y: y,
+		center: false,
+		frame: false,
+		transparent: true,
+		resizable: false,
+		minimizable: false,
+		maximizable: false,
+		closable: false,
+		alwaysOnTop: alwaysOnTop,
+		fullscreenable: false,
+		autoHideMenuBar: true,
+		icon: 'icon/logo.png',
+		// backgroundColor: '#2e2c29',
+	    webPreferences: {
+	      nodeIntegration: true,
+	      nodeIntegrationInWorker: true,
+	      nodeIntegrationInSubFrames: true
+	    }
+	});
+
+	if(env.NODE_ENV == 'devTools') {
+		win.webContents.openDevTools();
+	}
+	win.loadFile('src/renderer/index.html');
+}
+
+app.whenReady().then(createWindow);
