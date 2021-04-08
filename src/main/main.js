be2fa7301b7d590ead988508eae708e1aff4edf6
gaoyh <gaoyh@processon.com>
2021-04-08 16:37:06 +0800

    添加robot操控功能

--- src/main/main.js
+++ src/main/main.js
@@ -2,11 +2,13 @@ const { app, BrowserWindow, screen } = require('electron');
 const path = require('path');
 const fs = require('fs');
 const nodeApi = require('node-api');
+const robot = require("robotjs");
 const env = process.env;
 
 process.env.canvesWidth = 200;
 process.env.canvesHeight = 250;
 
+// 创建弹窗
 function createWindow() {
 	let workAreaSize = screen.getPrimaryDisplay().workAreaSize,
 		width = workAreaSize.width - 1,
@@ -48,8 +50,46 @@ function createWindow() {
 		win.webContents.openDevTools();
 	}
 	win.loadFile('src/renderer/index.html');
-}
+};
+// 控制鼠标
+function robotMouse() {
+	// Speed up the mouse.
+	robot.setMouseDelay(2);
+
+	var twoPI = Math.PI * 2.0;
+	var screenSize = robot.getScreenSize();
+	var height = (screenSize.height / 2) - 10;
+	var width = screenSize.width;
+
+	for (var x = 0; x < width; x++)
+	{
+		y = height * Math.sin((twoPI * x) / width) + height;
+		robot.moveMouse(x, y);
+	}
+};
+// 控制键盘
+function robotKeyBoard() {
+	console.log('robotKeyBoard')
+	// Type "Hello World".
+	robot.typeString("Hello World");
+
+	// Press enter.
+	robot.keyTap("enter");
+};
+// 获取屏幕
+function robotScreen() {
+	console.log('robotScreen')
+	// Get mouse position.
+	var mouse = robot.getMousePos();
+
+	// Get pixel color in hex format.
+	var hex = robot.getPixelColor(mouse.x, mouse.y);
+	console.log("#" + hex + " at x:" + mouse.x + " y:" + mouse.y);
+};
 
 app.on('ready', () => {
 	createWindow();
+	setTimeout(() => {
+		robotScreen();
+	}, 3000)
 })
